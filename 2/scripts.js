9bfe337b17a5a4936f8ef78079b3d9a964d64424
Oskar <hi@oskarradon.com>
2017-02-15 09:00:27 -0800

    Fix some stuff

--- 2/scripts.js
+++ 2/scripts.js
@@ -6,3 +6,17 @@ const camera = new THREE.PerspectiveCamera(
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
+
+function render() {
+  requestAnimationFrame( render );
+  cube.rotation.y += 0.01;
+  renderer.render( scene, camera );
+}
+render();
